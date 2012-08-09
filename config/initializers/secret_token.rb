64dbe7d2e200c678e19b4a0245394cc0659fb7ca
Mark Ryall <mark@ryall.name>
2012-08-09 17:12:23 +0800

    empty rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ocelots::Application.config.secret_token = 'd17f09fd42065bd3aa94c7514550b421d95a64ac9c8596b3dbfa8c23729e7d2634d66e86faf4e48ef6808723c0a2c84526d9e045d73b8bf8abc7056000ede7c9'
