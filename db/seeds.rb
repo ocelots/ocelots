89037a7076e200d02c8584b052cf05f4399406cd
Mark Ryall <mark@ryall.name>
2012-08-17 17:37:26 +0800

    Added some seed data) the originals

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# encoding: UTF-8
+
+Person.create account: 'liyanhui',
+    full_name: 'Li Yànhuī',
+    chinese_name: '李彦辉',
+    email: 'yhli@thoughtworks.com'
+
+Person.create account: 'tengyun',
+    full_name: 'Téng Yǔn',
+    chinese_name: '滕云',
+    preferred_name: 'Chinese Tom',
+    email: 'yteng@thoughtworks.com'
+
+Person.create account: 'wangbiao',
+    full_name: 'Wáng Biāo',
+    chinese_name: '王彪',
+    email: 'biaowang@thoughtworks.com'
+
+Person.create account: 'zhangyi',
+    full_name: 'Zhāng Yì',
+    chinese_name: '张逸',
+    email: 'yizhang@thoughtworks.com'
+
+Person.create account: 'markryall',
+    full_name: 'Mark Ryall',
+    pinyin_name: 'Mǎkè',
+    chinese_name: '马克',
+    email: 'mryall@thoughtworks.com'
