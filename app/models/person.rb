48fd35c73b6bbc1390d6957c81dc454ef802e92d
Mark Ryall <mark@ryall.name>
2012-08-17 16:41:42 +0800

    made audio mandatory and changed to slightly more convenient folder structure in s3

--- app/models/person.rb
+++ app/models/person.rb
@@ -14,21 +14,23 @@ class Person
       full_name: 'Li Yànhuī',
       chinese_name: '李彦辉',
       email: 'yhli@thoughtworks.com'
+
   add id: 'tengyun',
       full_name: 'Téng Yǔn',
       chinese_name: '滕云',
       preferred_name: 'Chinese Tom',
-      email: 'yteng@thoughtworks.com',
-      audio: 'tengyun'
+      email: 'yteng@thoughtworks.com'
+
   add id: 'wangbiao',
       full_name: 'Wáng Biāo',
       chinese_name: '王彪',
-      email: 'biaowang@thoughtworks.com',
-      audio: 'wangbiao'
+      email: 'biaowang@thoughtworks.com'
+
   add id: 'zhangyi',
       full_name: 'Zhāng Yì',
       chinese_name: '张逸',
       email: 'yizhang@thoughtworks.com'
+
   add id: 'markryall',
       full_name: 'Mark Ryall',
       pinyin_name: 'Mǎkè',
