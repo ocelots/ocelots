35581f74b290c87f316e62c47d4addb16ff7f59c
nparis <nparis@thoughtworks.com>
2012-08-24 23:06:36 +1000

    forgot to add updating already existing users with the persona_id

--- db/migrate/20120824110923_add_persona_id_to_person.rb
+++ db/migrate/20120824110923_add_persona_id_to_person.rb
@@ -1,5 +1,9 @@
 class AddPersonaIdToPerson < ActiveRecord::Migration
   def change
     add_column :people, :persona_id, :string
+
+    Person.find(:all).each do |p|
+      p.update_attribute :persona_id, Digest::MD5.hexdigest(p.email)
+    end
   end
 end
