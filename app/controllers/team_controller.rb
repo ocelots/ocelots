425ef56200fc2a254c08c912b7a0206993d8f45c
Mark Ryall <mark@ryall.name>
2012-08-27 22:44:10 +1000

    restricted people from seeing teams they are not in

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -1,6 +1,7 @@
 class TeamController < ApplicationController
   def show
     @team = Team.find_by_slug params[:slug]
+    @team = nil unless current_person.teams.include? @team
     render :unknown_team unless @team
   end
 end
