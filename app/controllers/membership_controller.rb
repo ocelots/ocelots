863ca16bfc546cd0736583e18533735d3d49bc57
Mark Ryall <mark@ryall.name>
2012-08-30 22:34:51 +1000

    added some logging to membership controller

--- app/controllers/membership_controller.rb
+++ app/controllers/membership_controller.rb
@@ -11,11 +11,15 @@ class MembershipController < ApplicationController
 private
   def process_sc_embed_code membership
     return unless membership
+    logger.info "attempting to attach soundcloud information to membership #{membership.id}"
     return unless membership.person == current_person
     if params[:sc_embed_code] =~ /tracks%2F(\d+)%3Fsecret_token%3D([^&]+)&/
+      logger.info "matched soundcloud track #{$1} and secret #{$2}"
       membership.track = $1
       membership.secret = $2
       membership.save
+    else
+      logger.info 'no match'
     end
   end
 end
