2456044063beff1b20d6dc8d15c1020a28225f6c
Mark Ryall <mark@ryall.name>
2012-09-10 15:25:52 +1000

    added a check for profile to hide people who you do not share a team with

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     return nil unless session[:email]
+    return @current_user if @current_user
     @current_user = session[:email]
     if params[:override] && Person.omnipotent?(@current_user)
       @current_user = session[:email] = params[:override]
@@ -24,8 +25,9 @@ class ApplicationController < ActionController::Base
   end
 
   def current_person
-    @person = Person.find_by_email @current_user
-    @person = Person.create email: @current_user unless @person
-    @person
+    return @current_person if @current_person
+    @current_person = Person.find_by_email current_user
+    @current_person = Person.create email: current_user unless @current_person
+    @current_person
   end
 end
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,9 @@
 class ProfilesController < ApplicationController
   def show
     @person = Person.find_by_account params[:account]
+    unless current_person.blessed?
+      @person = nil if (current_person.teams & @person.teams).empty?
+    end
     render :unknown unless @person
   end
 end
