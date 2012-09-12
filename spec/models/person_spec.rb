3a96fec032ad97bfa63f2cf712744e3e1c44fa5f
Mark Ryall <mark@ryall.name>
2012-09-13 09:15:39 +1000

    added spec for auto generated account and auth_token person attributes

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -12,8 +12,23 @@ describe Person do
   end
 
   describe '#create_for_email' do
+    let(:email) { 'user@email.com' }
+    let(:created_person) { Person.create_for_email(email) }
+
     it 'should generate a valid person' do
-      Person.create_for_email('user@email.com').should be_valid
+      created_person.should be_valid
+    end
+
+    it 'should calculate persona_id from email' do
+      created_person.persona_id.should == Digest::MD5.hexdigest(email)
+    end
+
+    it 'should automaticaly populate auth_token' do
+      created_person.auth_token.should =~ /\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/
+    end
+
+    it 'should automaticaly populate account' do
+      created_person.account.should =~ /\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/
     end
   end
 end
