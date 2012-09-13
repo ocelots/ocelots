c9128292b022f80d6d8a31a4a7fd0aadfe5f37fa
Mark Ryall <mark@ryall.name>
2012-09-14 08:47:54 +1000

    added check to see if person was found before checking whether they share a team with current user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
   def show
     @person = Person.find_by_account params[:account]
     unless current_person.blessed?
-      @person = nil if (current_person.teams & @person.teams).empty?
+      @person = nil if @person and (current_person.teams & @person.teams).empty?
     end
     render :unknown unless @person
   end
