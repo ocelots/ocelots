aa6cd09918ad13841677623c63965c949f1fb6c8
Mark Ryall <mark@ryall.name>
2012-09-24 22:49:29 +1000

    made all phone numbers tel links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def image_for person
     image_tag person.photo? ? person.photo.url(:square) : '/assets/silhouette.png'
   end
+
+  def phone_link_to phone
+    phone.blank? ? '' : link_to(phone,"tel://#{phone.gsub(/\D/,'')}")
+  end
 end
