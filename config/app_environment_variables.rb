faca6ff97fb9eabf4c6de5d063704225792d79aa
szpyxlwoni <blasphemymoon@gmail.com>
2012-11-16 16:09:17 +0800

    change client secret

--- config/app_environment_variables.rb
+++
@@ -1,3 +0,0 @@
-ENV['GOOGLE_OAUTH_CLIENT_ID '] = '1030433741080-bqra8568mng0bfor1u1q5k7iobl2e6bh.apps.googleusercontent.com'
-ENV['GOOGLE_OAUTH_CLIENT_SECRET '] = '0Wf3ur7LvMaj2qR-S-UTWOrW'
-ENV['GOOGLE_OAUTH_REDIRECT '] = 'http://localhost:3000/home/verify_g_callback'
--- lib/google_api_ext.rb
+++ lib/google_api_ext.rb
@@ -4,9 +4,9 @@ Google::APIClient.class_eval do
   def self.build
     client = Google::APIClient.new
     client.authorization.scope = 'https://www.googleapis.com/auth/userinfo.email'
-    client.authorization.client_id = ENV['GOOGLE_OAUTH_CLIENT_ID ']
-    client.authorization.client_secret = ENV['GOOGLE_OAUTH_CLIENT_SECRET ']
-    client.authorization.redirect_uri = ENV['GOOGLE_OAUTH_REDIRECT ']
+    client.authorization.client_id = ENV['GOOGLE_OAUTH_CLIENT_ID']
+    client.authorization.client_secret = ENV['GOOGLE_OAUTH_CLIENT_SECRET']
+    client.authorization.redirect_uri = ENV['GOOGLE_OAUTH_REDIRECT']
     client
   end
 end
