9b8586e9f25d8ada545e01f87cdab1bfef4e26d5
szpyxlwoni <blasphemymoon@gmail.com>
2012-11-16 15:52:00 +0800

    change client secret

--- lib/google_api_ext.rb
+++ lib/google_api_ext.rb
@@ -1,34 +1,12 @@
 require "google/api_client"
 
 Google::APIClient.class_eval do
-  def self.build(origin = 'localhost:3000')
-    configurations = [
-        {
-            origin: 'localhost:3000',
-            client_id: '1030433741080-bqra8568mng0bfor1u1q5k7iobl2e6bh.apps.googleusercontent.com',
-            client_secret: 'u6IrqRnwW7OomwLSVGERssQy',
-            redirect_uri: 'http://localhost:3000/home/verify_g_callback'
-        },
-        {
-            origin: 'ocelots-staging.herokuapp.com',
-            client_id: '1030433741080-o6q68rkma5vrj64as9omlfur1ii90ftq.apps.googleusercontent.com',
-            client_secret: 'abKVdtTIYbXapUkEwQ4Lt9VG',
-            redirect_uri: 'http://ocelots-staging.herokuapp.com/home/verify_g_callback'
-        },
-        {
-            origin: 'iocelots.com',
-            client_id: '1030433741080.apps.googleusercontent.com',
-            client_secret: 'RAQdt17GKweBtQzOCq6Dp965',
-            redirect_uri: 'http://iocelots.com/home/verify_g_callback'
-        }
-    ]
-    client_configuration = configurations.find{|conf| origin.include?(conf[:origin])}
-
+  def self.build
     client = Google::APIClient.new
     client.authorization.scope = 'https://www.googleapis.com/auth/userinfo.email'
-    client.authorization.client_id = client_configuration[:client_id]
-    client.authorization.client_secret = client_configuration[:client_secret]
-    client.authorization.redirect_uri = client_configuration[:redirect_uri]
+    client.authorization.client_id = ENV['GOOGLE_OAUTH_CLIENT_ID ']
+    client.authorization.client_secret = ENV['GOOGLE_OAUTH_CLIENT_SECRET ']
+    client.authorization.redirect_uri = ENV['GOOGLE_OAUTH_REDIRECT ']
     client
   end
 end
--- spec/lib/google_api_client_spec.rb
+++ spec/lib/google_api_client_spec.rb
@@ -7,14 +7,14 @@ describe Google::APIClient do
       client.authorization.redirect_uri.to_s.should == 'http://localhost:3000/home/verify_g_callback'
     end
 
-    it 'builds google api client for staging' do
-      client = Google::APIClient.build('ocelots-staging.herokuapp.com')
-      client.authorization.redirect_uri.to_s.should == 'http://ocelots-staging.herokuapp.com/home/verify_g_callback'
-    end
-
-    it 'builds google api client for production' do
-      client = Google::APIClient.build('iocelots.com')
-      client.authorization.redirect_uri.to_s.should == 'http://iocelots.com/home/verify_g_callback'
-    end
+    #it 'builds google api client for staging' do
+    #  client = Google::APIClient.build
+    #  client.authorization.redirect_uri.to_s.should == 'http://ocelots-staging.herokuapp.com/home/verify_g_callback'
+    #end
+    #
+    #it 'builds google api client for production' do
+    #  client = Google::APIClient.build
+    #  client.authorization.redirect_uri.to_s.should == 'http://iocelots.com/home/verify_g_callback'
+    #end
   end
 end
