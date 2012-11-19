e64f89c470231ee971fc68f2b74a076d41ea2a9d
Mark Ryall <mark@ryall.name>
2012-11-20 00:40:16 +1100

    moved google oauth stuff into a mixin and removed unnecesary serialisation to the session

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,11 @@
 require 'persona'
+require 'google_oauth'
 require 'net/http'
 require 'google_api_ext'
 
 class HomeController < ApplicationController
   include Persona
+  include GoogleOauth
 
   skip_filter :require_login, only: [:index, :verify, :verify_g, :verify_g_callback]
 
@@ -30,26 +32,11 @@ class HomeController < ApplicationController
   end
 
   def verify_g
-    @client = Google::APIClient.build
-    url = @client.authorization.authorization_uri.to_s
-    session[:google_auth] = @client.to_yaml
-
-    redirect_to url
+    redirect_to google_oauth_url
   end
 
   def verify_g_callback
-    google_auth = YAML.load(session[:google_auth])
-    google_auth.authorization.code = params[:code] if params[:code]
-
-    google_auth.authorization.fetch_access_token!
-
-    http = Net::HTTP.new 'www.googleapis.com', 443
-    http.use_ssl = true
-    response = http.get "/oauth2/v1/userinfo?access_token=#{google_auth.authorization.access_token}"
-
-    response = JSON.parse response.body
-    session[:email] = response['email']
+    verify_google_oauth { |email| session[:email] = email }
     redirect_to '/'
   end
-
 end
