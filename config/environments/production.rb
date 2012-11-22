d8cad08b255a861733c40f5b10b86feb31c73a9e
Junv <wahyd4@gmail.com>
2012-11-22 18:16:24 +0800

    sss

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Ocelots::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile << '*.js.coffee'
+  config.assets.precompile << '*.js'
   config.assets.precompile += %w( style.css )
 
   # Disable delivery errors, bad email addresses will be ignored
